1d0240ed9e5c070f7737cc1ce8c4939c430eca3e
KovalevAnton <kooovaaal@gmail.com>
2018-10-23 09:07:09 +0300

    add add-chat button

--- src/components/ChatList/index.tsx
+++ src/components/ChatList/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { ChatListItem } from "./ChatItem";
 import { connect } from "react-redux";
-import { ScrollView, Animated, Dimensions, View } from "react-native";
+import { ScrollView, Animated, Dimensions, View, TouchableOpacity, Text } from "react-native";
 import styled from "styled-components";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { logout } from "../../actions/auth";
-import { WHITE_COLOR } from "../../helpers/styleConstants";
+import { WHITE_COLOR, SOFT_BLUE_COLOR, BLACK_COLOR } from "../../helpers/styleConstants";
 import { getMessages, setActiveChat } from "../../actions/chat";
 import { Navigation } from "react-native-navigation";
 import ChatMenu from "../ChatMenu";
@@ -27,7 +28,10 @@ interface IProps {
 
 interface IState {
   isMenuOpen: boolean;
+  isAddChatButtonVisible: boolean;
   animated: any;
+  addChatButtonAnimate: any;
+  currentChatMenuScrollPosition: number;
 }
 
 class ChatList extends React.Component<IProps, IState> {
@@ -36,6 +40,9 @@ class ChatList extends React.Component<IProps, IState> {
     this.state = {
       isMenuOpen: false,
       animated: new Animated.Value(0),
+      isAddChatButtonVisible: true,
+      addChatButtonAnimate: new Animated.Value(0),
+      currentChatMenuScrollPosition: 0,
     };
   }
 
@@ -46,6 +53,31 @@ class ChatList extends React.Component<IProps, IState> {
       duration: 500,
     }).start();
   };
+
+  public showAddChatButton = () => {
+    this.setState({ isAddChatButtonVisible: true })
+    Animated.timing(this.state.addChatButtonAnimate, {
+      toValue: 0,
+      duration: 500,
+    }).start();
+  };
+
+  public closeAddChatButton = () => {
+    Animated.timing(this.state.addChatButtonAnimate, {
+      toValue: 1,
+      duration: 500,
+    }).start(() => this.setState({ isAddChatButtonVisible: false }));
+  };
+
+  public toggleAddChatButton = (event) => {
+    const { currentChatMenuScrollPosition } = this.state
+    if (currentChatMenuScrollPosition < event.nativeEvent.contentOffset.y) {
+      this.closeAddChatButton()
+    } else {
+      this.showAddChatButton()
+    }
+    this.setState({ currentChatMenuScrollPosition: event.nativeEvent.contentOffset.y })
+  }
 
   public closeChatMenu = () => {
     Animated.timing(this.state.animated, {
@@ -90,7 +122,8 @@ class ChatList extends React.Component<IProps, IState> {
               })}
             leftIconName="align-left"
             rightIconName="user" />
-          <ScrollView>
+          <ScrollView
+            onScrollBeginDrag={(event) => this.toggleAddChatButton(event)}>
             {this.props.chat.chats.map(chat => (
               <ChatListItem
                 navigateToChat={() =>
@@ -116,8 +149,25 @@ class ChatList extends React.Component<IProps, IState> {
               />
             ))}
           </ScrollView>
+          <AddChatButton style={{
+            display: this.state.isAddChatButtonVisible ? "flex" : 'none',
+            transform: [{
+              translateY: this.state.addChatButtonAnimate.interpolate(
+                  {
+                      inputRange: [0, 1],
+                      outputRange: [0, 100],
+                  }
+              )
+          }]
+          }}>
+            <Icon
+              size={22}
+              name="pen"
+              backgroundColor={WHITE_COLOR}
+              color={WHITE_COLOR} />
+          </AddChatButton>
         </ChatListWrapper>
-      </View>
+      </View >
     );
   }
 }
@@ -129,6 +179,20 @@ const ChatListWrapper = styled(View)`
         borderLeftWidth: 3;
         borderColor: rgba(0,0,0,0.05);
         height: 100%;
+        position: relative;
+      `;
+
+const AddChatButton = styled(Animated.View)`
+        padding: 20px;
+        position: absolute;
+        right: 20;
+        bottom: 20;
+        backgroundColor: ${SOFT_BLUE_COLOR};
+        border-radius: 50;
+        shadowRadius: 3; 
+        shadowOpacity: 0.2; 
+        shadowOffset: {width: 1, height: 1}; 
+        shadowColor: ${BLACK_COLOR};
       `;
 
 const mapStateToProps = state => ({ auth: state.auth, chat: state.chat });
