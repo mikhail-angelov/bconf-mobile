2bce62c3101996d57336a31cce4b615a60d60200
Olga Tsytserova <helgamir@gmail.com>
2018-10-30 09:37:20 +0300

    react native upgrade to v57 (ios) (#17)
    * upgrade react native to 57 (ios)
    
    * fix formatting; reset gitignore

--- android/app/src/main/java/com/mobiledemo/MainActivity.java
+++ android/app/src/main/java/com/mobiledemo/MainActivity.java
@@ -1,8 +1,8 @@
 package com.mobiledemo;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends NavigationActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  getTransformModulePath() {
-    return require.resolve("react-native-typescript-transformer");
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-typescript-transformer'),
   },
-  getSourceExts() {
-    return ["ts", "tsx"];
-  }
 };
