a7bc0f866e472ed6f6d6f2d4d9eb5271ffffefaf
Olga Tsytserova <helgamir@gmail.com>
2018-11-28 15:35:26 +0300

    add request permissions and fcm token acquiring

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,54 @@
 import React from "react";
 import { connect } from "react-redux";
 import _ from "lodash";
-import { ActivityIndicator, View } from "react-native";
+import { ActivityIndicator, View, AsyncStorage } from "react-native";
 import { checkAuth } from "../actions/auth";
 import { BLACK_COLOR } from "../helpers/styleConstants";
 import styled from "styled-components";
+import firebase from "react-native-firebase";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     props.checkAuth();
+  }
+  public async componentDidMount() {
+    this.checkPermission();
+  }
+
+  //1
+  public async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  }
+
+  //3
+  public async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        console.log("FSM token:", fcmToken)
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+  }
+
+  //2
+  public async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+    }
   }
   public render() {
     return (
@@ -24,7 +63,7 @@ const mapDispatchToProps = {
   checkAuth
 };
 
-const mapStateToProps = ({auth}) => ({ auth });
+const mapStateToProps = ({ auth }) => ({ auth });
 
 export default connect(
   mapStateToProps,
